b00cc17bb7c574b4bfc9785979f870f2597e6261
Ben Loper <loperb06@gmail.com>
2019-01-25 15:14:27 -0500

    Starting over from scratch

--- HoneyDoListCLI/HoneyDoListCLI/Program.cs
+++ HoneyDoListCLI/HoneyDoListCLI/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using HoneyDoListCLI.Classes;
+﻿
+using System;
 
 namespace HoneyDoListCLI
 {
@@ -8,36 +7,8 @@ namespace HoneyDoListCLI
     {
         public static void Main(string[] args)
         {
-            Project electrical = new Project("Electrical", "Replace the lights in the kitchen and recepticals", false);
-            Project hoseBib = new Project("Hose Bib", "Replace backyard hose bib", true);
-            Project replaceCarpet = new Project("Carpets", "Replace all the carpet in each room", false);
-            Project replaceCeilingFan = new Project("Ceiling Fan", "Replace the ceiling fan in enterance with a chandelier", false);
-            Project fixSlidingGlassDoor = new Project("Sliding Glass Door", "Replace the wheels on the sliding glass door and repair the tracks", true);
-
-            ProjectList listOfProjects = new ProjectList();
-
-            listOfProjects.addProject(electrical);
-            listOfProjects.addProject(hoseBib);
-            listOfProjects.addProject(replaceCarpet);
-            listOfProjects.addProject(replaceCeilingFan);
-            listOfProjects.addProject(fixSlidingGlassDoor);
-
-            foreach(Project item in listOfProjects.addedProjects)
-            {
-                string finished = "No";
-                if(item.IsCompleted == true)
-                {
-                    finished = "Yes";
-                }
-
-                Console.WriteLine($"Project Title: {item.ProjectTitle}\n");
-                Console.WriteLine($"Project Description: {item.ProjectDescription}\n");
-                Console.WriteLine($"Project finished: {finished}\n");
-                Console.WriteLine("***********************************\n");
-            }
-
+            Console.WriteLine("Starting over");
             Console.ReadKey();
         }
-
     }
 }
